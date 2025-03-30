d8b66600d5f05e5e9246953a8ee93e0eb32021b3
Jon Bergland <jobergl@stud.ntnu.no>
2025-03-30 12:13:20 +0200

    feat: create and initialize LogInForm.vue

---
+++ frontend/src/components/LogIn/LogInForm.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import "@/assets/color.css"
+import "@/assets/base.css"
+</script>
+
+<template>
+  <div class="login-form" id="login-form">
+    <form action="">
+      <label for="login">Please enter email and password:</label>
+      <input class="input" type="text" id="email" name="email" placeholder="Email" required />
+      <input class="input" type="text" id="password" name="password" placeholder="Password" required/>
+      <input type="submit" value="Log in" id="login-button">
+    </form>
+  </div>
+</template>
+
+<style>
+
+.login-form {
+  display: grid;
+  grid-template-columns: auto;
+  place-items: center;
+  border: 1px solid var(--color-border);
+  padding: 10px;
+  border-radius: 8px;
+  background-color: var(--color-background-soft);
+}
+
+.login-form form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
+
+#login-button {
+  background-color: var(--color-primary);
+  color: #FFFFFF;
+  border-radius: var(--size-button-radius);
+  padding: var(--size-button-padding);
+  min-width: var(--size-button-min);
+}
+</style>
