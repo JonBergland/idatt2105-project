126be9f6d934fa851e5da3c162e0e2f844293abd
Jon Bergland <jobergl@stud.ntnu.no>
2025-03-30 12:13:42 +0200

    feat: init and create LogInView.vue

---
+++ frontend/src/views/LogIn/LogInView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import LogIn from "../../components/LogIn/LogInForm.vue";
+</script>
+
+<template>
+ <div class="login-wrapper">
+  <h1>Log In</h1>
+  <LogIn />
+  <h4>or</h4>
+  <button class="signup-button">Sign up</button>
+ </div>
+
+</template>
+
+<style>
+.login-wrapper {
+  display: grid;
+  grid-template-columns: auto;
+  place-items: center;
+  color: var(--color-heading);
+  gap: 5px;
+}
+
+.signup-button {
+  background-color: var(--color-secondary);
+  width: auto;
+}
+</style>
