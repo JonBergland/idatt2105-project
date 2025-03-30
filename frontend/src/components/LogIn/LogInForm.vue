8dee38672eec815f6a7873528634bd55bcef5d69
Jon Bergland <jobergl@stud.ntnu.no>
2025-03-30 13:07:25 +0200

    feat: add handleLogin function

--- frontend/src/components/LogIn/LogInForm.vue
+++ frontend/src/components/LogIn/LogInForm.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import "@/assets/color.css"
 import "@/assets/base.css"
+
+const email = ref("")
+const password = ref("")
+
+async function handleLogin() {
+  const formData = {
+    email: email.value,
+    password: password.value
+  }
+
+  try {
+    // TODO: Add form submission logic
+    console.log(formData);
+
+  } catch (error) {
+    const errorLabel = document.querySelector('label[id=login-status-label]')
+    if (errorLabel != null) errorLabel.textContent = "Log in failed"
+    console.log("Error when logging in: ", error);
+  }
+}
+
 </script>
 
 <template>
   <div class="login-form" id="login-form">
-    <form action="">
+    <form @submit.prevent="handleLogin">
       <label for="login">Please enter email and password:</label>
-      <input class="input" type="text" id="email" name="email" placeholder="Email" required />
-      <input class="input" type="text" id="password" name="password" placeholder="Password" required/>
+      <input class="input" type="text" id="email" name="email" placeholder="Email" v-model="email" required />
+      <input class="input" type="text" id="password" name="password" placeholder="Password" v-model="password" required/>
       <input type="submit" value="Log in" id="login-button">
+      <label for="error" id="login-status-label"></label>
     </form>
   </div>
 </template>
@@ -40,4 +63,9 @@ import "@/assets/base.css"
   padding: var(--size-button-padding);
   min-width: var(--size-button-min);
 }
+
+#login-button:hover {
+  background-color: var(--color-primary-hover);
+}
+
 </style>
