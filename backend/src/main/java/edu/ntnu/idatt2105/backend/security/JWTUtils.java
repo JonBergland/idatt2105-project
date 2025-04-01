6ddb437e850b2d7cb7d35859d0989af1c182301e
Vetle <vetletb@stud.ntnu.no>
2025-04-01 18:16:25 +0200

    feat: created JWTUtils and token validation method

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/security/JWTUtils.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idatt2105.backend.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTUtils {
+
+  private static final String KEY_SECRET = "04035bd7735d40a293ab588b5471d939";
+  private static final Duration JWT_VALIDITY = Duration.ofMinutes(5);
+
+  private final Logger logger = LoggerFactory.getLogger(JWTAuthorizationFilter.class);
+
+  private DecodedJWT validateToken(final String token) throws JWTVerificationException {
+    try {
+      final Algorithm hmac512 = Algorithm.HMAC512(KEY_SECRET);;
+      final JWTVerifier verifier = JWT.require(hmac512).build();
+      return verifier.verify(token);
+    } catch (final JWTVerificationException e) {
+      logger.warn("token is invalid {}", e.getMessage());
+      throw e;
+    }
+  }
+}
