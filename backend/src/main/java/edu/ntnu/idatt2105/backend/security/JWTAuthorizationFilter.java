5329e3c35bed691473e40f0d4dfc18d11d9a9102
Vetle <vetletb@stud.ntnu.no>
2025-04-01 18:21:32 +0200

    feat: created JWTAuthorizationFilter with jwt authentication logic

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/security/JWTAuthorizationFilter.java
@@ -0,0 +1,72 @@
+package edu.ntnu.idatt2105.backend.security;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+  private final Logger logger = LoggerFactory.getLogger(JWTAuthorizationFilter.class);
+
+  private final JWTUtils jwtUtils;
+
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    logger.info("JWTAuthorizationFilter called for URI: {}", request.getRequestURI());
+    final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (header == null || !header.startsWith("Bearer ")) {
+      logger.warn("Token has wrong format: {}", header);
+      filterChain.doFilter(request, response);
+      return;
+    }
+    String token = header.substring(7);
+
+    final String username;
+    final String role;
+    logger.info("Validating token");
+    try {
+      username = jwtUtils.validateTokenAndGetUserId(token);
+      role = jwtUtils.validateTokenAndGetRole(token);
+    } catch (IllegalArgumentException | JWTVerificationException e) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    if (username == null || role == null) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    List<SimpleGrantedAuthority> authorities = Collections.singletonList(
+        new SimpleGrantedAuthority(role));
+    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+        username,
+        null,
+        authorities);
+    SecurityContextHolder.getContext().setAuthentication(auth);
+
+    logger.info("user: {}, role: {}, has been authenticated", username, role);
+    filterChain.doFilter(request, response);
+  }
+}
