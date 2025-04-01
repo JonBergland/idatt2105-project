a036fbdee1b487f6e668cc0c7dd0d1ca8967bc99
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-01 09:47:50 +0200

    feat: add signup to routing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LogIn/LogInView.vue'
+import SignUpView from '@/views/SignUp/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView,
     },
   ],
 })
