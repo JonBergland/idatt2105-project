8555dfea05c275a894b6b19f8c52d3f65048b756
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-01 09:54:43 +0200

    feat: create Signup view with SignUpForm-component

---
+++ frontend/src/views/SignUp/SignUpView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import SignUpForm from '@/components/SignUp/SignUpForm.vue';
+
+</script>
+
+<template>
+<div class="signup-wrapper">
+  <h1>Welcome to the Yard!</h1>
+  <SignUpForm />
+ </div>
+
+</template>
+
+<style>
+.login-wrapper {
+  display: grid;
+  grid-template-columns: auto;
+  place-items: center;
+  color: var(--color-heading);
+  gap: 5px;
+}
+</style>
