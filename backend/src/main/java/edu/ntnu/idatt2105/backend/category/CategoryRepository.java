9fe720e7925be34f143d27856d577e2c86e06a5c
Vetle <vetletb@stud.ntnu.no>
2025-04-02 11:46:45 +0200

    docs: added Javadoc to CategoryRepository

--- backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryRepository.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryRepository.java
@@ -2,18 +2,27 @@ package edu.ntnu.idatt2105.backend.category;
 
 import edu.ntnu.idatt2105.backend.category.model.Category;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for updating, deleting and inserting to the Category table.
+ */
 @Repository
 @RequiredArgsConstructor
 public class CategoryRepository {
 
   private final JdbcTemplate jdbcTemplate;
 
-  public Category getCategories() {
+  /**
+   * gets the categories from the database.
+   *
+   * @return the categories
+   * @throws DataAccessException if something goes wrong
+   */
+  public Category getCategories() throws DataAccessException {
     List<String> categories = jdbcTemplate.queryForList(
         "SELECT category_name FROM Categories",
         String.class);
