c2f74aa00d3088f578c311877d891a467c9db077
Vetle <vetletb@stud.ntnu.no>
2025-04-02 09:27:46 +0200

    docs: added Javadoc to UserMapper

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/UserMapper.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/UserMapper.java
@@ -7,10 +7,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * A Mapper for mapping between User and DTOs.
+ */
 @Mapper
 public interface UserMapper {
   UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+  /**
+   * Maps signupRequest dto to user model.
+   *
+   * @param signupRequest the dto to map
+   * @return the mapped user model
+   */
   @Mapping(source = "email", target = "email")
   @Mapping(source = "password", target = "password")
   @Mapping(source = "name", target = "name")
@@ -18,6 +27,12 @@ public interface UserMapper {
   @Mapping(source = "phoneNumber", target = "phoneNumber")
   User signupRequestToUser(SignupRequest signupRequest);
 
+  /**
+   * Maps signinRequest dto to user model.
+   *
+   * @param signinRequest the dto to map
+   * @return the mapped user model
+   */
   @Mapping(source = "email", target = "email")
   @Mapping(source = "password", target = "password")
   User signinRequestToUser(SigninRequest signinRequest);
