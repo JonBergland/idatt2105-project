3c11d19f510246b93e31b9896f2a633b44e48cd9
Vetle <vetletb@stud.ntnu.no>
2025-04-02 14:06:56 +0200

    feat: created ItemsResponse

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/dto/ItemsResponse.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2105.backend.item.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemsResponse {
+  ItemResponse[] items;
+}
