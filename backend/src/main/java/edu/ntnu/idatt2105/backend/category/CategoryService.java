5822c6e1bd9c8daabd5f56c56da1b33922804544
Vetle <vetletb@stud.ntnu.no>
2025-04-02 11:46:57 +0200

    docs: added Javadoc to CategoryService

--- backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryService.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryService.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for the Category model.
+ */
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
@@ -14,6 +17,11 @@ public class CategoryService {
 
   private final CategoryRepository categoryRepository;
 
+  /**
+   * gets all categories.
+   *
+   * @return the category object
+   */
   public Category getCategories() {
     return categoryRepository.getCategories();
   }
