b6506d1d3bbac50a88553ac5e3738f4d4df33b5c
Vetle <vetletb@stud.ntnu.no>
2025-04-02 10:41:16 +0200

    feat: created CategoryController with test endpoint

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryController.java
@@ -0,0 +1,25 @@
+package edu.ntnu.idatt2105.backend.category;
+
+import edu.ntnu.idatt2105.backend.category.dto.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/store/category")
+@CrossOrigin
+@RequiredArgsConstructor
+public class CategoryController {
+
+  private final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+
+  @GetMapping()
+  public CategoryResponse getCategories() {
+    String[] test = {"mat", "elektronikk", "klær"};
+    return new CategoryResponse(test);
+  }
+}
