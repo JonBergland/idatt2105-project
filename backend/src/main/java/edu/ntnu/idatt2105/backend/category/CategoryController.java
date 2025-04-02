2a1b1d297f2af64cb200a62303ed0f81150e84f7
Vetle <vetletb@stud.ntnu.no>
2025-04-02 11:48:02 +0200

    feat: added exception handling for bad db connection

--- backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryController.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/CategoryController.java
@@ -5,10 +5,13 @@ import edu.ntnu.idatt2105.backend.category.model.Category;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/api/store/category")
@@ -22,7 +25,13 @@ public class CategoryController {
 
   @GetMapping()
   public CategoryResponse getCategories() {
-    Category category = categoryService.getCategories();
-    return new CategoryResponse(category.getCategories());
+    try {
+      logger.info("retrieves categories from database");
+      Category category = categoryService.getCategories();
+      return new CategoryResponse(category.getCategories());
+    } catch (DataAccessException e) {
+      logger.error("could not get categories from db");
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
