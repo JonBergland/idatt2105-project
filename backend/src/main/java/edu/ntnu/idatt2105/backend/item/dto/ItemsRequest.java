2dea5982f9aaa8147c25d9366689726f6782fb10
Vetle <vetletb@stud.ntnu.no>
2025-04-02 14:07:37 +0200

    feat: created ItemsRequest

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/dto/ItemsRequest.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2105.backend.item.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ItemsRequest {
+  String category;
+  String searchWord;
+  int[] priceMinMax;
+  int page;
+  int pageSize;
+}
