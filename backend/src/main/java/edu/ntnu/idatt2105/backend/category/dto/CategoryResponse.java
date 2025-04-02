d95a170bda3fca525378473c6e68a453f1dfdea8
Vetle <vetletb@stud.ntnu.no>
2025-04-02 11:46:29 +0200

    docs: added Javadoc to CategoryResponse

--- backend/src/main/java/edu/ntnu/idatt2105/backend/category/dto/CategoryResponse.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/dto/CategoryResponse.java
@@ -3,6 +3,9 @@ package edu.ntnu.idatt2105.backend.category.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * A DTO for responding to get category request.
+ */
 @Getter
 @AllArgsConstructor
 public class CategoryResponse {
