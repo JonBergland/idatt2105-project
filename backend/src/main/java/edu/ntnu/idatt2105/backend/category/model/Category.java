68f89b3a7b48117f50654e04e5a787593e8acbf9
Vetle <vetletb@stud.ntnu.no>
2025-04-02 11:07:03 +0200

    feat: created Category model

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/model/Category.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2105.backend.category.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Category {
+  String[] categories;
+}
