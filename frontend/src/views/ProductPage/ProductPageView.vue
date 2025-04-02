cebb3832cee75caf1658d93f39e994cddb4b1c38
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-02 10:30:18 +0200

    feat: create ProductPageView to show product info

---
+++ frontend/src/views/ProductPage/ProductPageView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import ProductNameComponent from '@/components/ProductPage/ProductNameComponent.vue'
+import { onMounted, ref,  } from 'vue';
+
+const props = defineProps<{
+  productId: string
+}>()
+
+const productName = ref("")
+const productState = ref("")
+const productComment = ref("")
+const sellerName = ref("")
+const sellerLocation = ref("")
+
+async function loadProduct(productId:string) {
+  // Retrive information from API-call
+  // TODO: Fix api-call
+
+  // Imitate API-call
+  productName.value = "Product name"
+  productState.value = "Good"
+  productComment.value = "In a good condition. Hardly used"
+  sellerName.value = "Ola Nordman"
+  sellerLocation.value = "Bloksberg"
+
+}
+
+onMounted(() => {
+  loadProduct(props.productId)
+})
+
+</script>
+
+<template>
+  <div class="product-page-wrapper">
+    <ProductNameComponent
+    :product-name="productName"
+    />
+  </div>
+
+</template>
