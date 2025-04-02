cb12ad74c4446bf5d8463806f6553c65a9eac06c
Vetle <vetletb@stud.ntnu.no>
2025-04-02 10:40:50 +0200

    feat: created CategoryResponse

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/category/dto/CategoryResponse.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idatt2105.backend.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CategoryResponse {
+  String[] categories;
+}
