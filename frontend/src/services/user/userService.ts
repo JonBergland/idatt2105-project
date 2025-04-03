aa101d777b59672a91e9848a62275766babfe202
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-03 08:43:39 +0200

    feat: add UserService-class for communicating with the backend

---
+++ frontend/src/services/user/userService.ts
@@ -0,0 +1,18 @@
+import type { UserLoginDTO, UserRegistrationDTO } from "@/models/user";
+import axiosInstance from "../axiosService";
+
+
+class UserService {
+
+  getUserInfo() {
+    // TODO
+  }
+
+  registerUser(user: UserRegistrationDTO) {
+    // TODO
+  }
+
+  loginUser(user: UserLoginDTO) {
+    // TODO
+  }
+}
