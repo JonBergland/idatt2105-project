d02d36eaa7bbddea6ab27b74472571d600a72eb0
florianleucht <fsleucht@stud.ntnu.no>
2025-04-03 13:02:19 +0200

    feat: added route from home page to result page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home/HomeView.vue'
 import LoginView from '@/views/LogIn/LogInView.vue'
 import SignUpView from '@/views/SignUp/SignUpView.vue'
+import ResultView from '@/views/Result/ResultView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignUpView,
+    },
+    {
+      path: '/result',
+      name: 'result',
+      component: ResultView,
     },
   ],
 })
