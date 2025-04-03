4256455096be7f9fef2ad153660f7b384c1f61ff
Vetle <vetletb@stud.ntnu.no>
2025-04-03 09:55:14 +0200

    docs: added Javadoc to ItemService

--- backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemService.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemService.java
@@ -7,16 +7,31 @@ import edu.ntnu.idatt2105.backend.item.model.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for the Item model.
+ */
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
   private final ItemRepository itemRepository;
 
+  /**
+   * gets items based on filter.
+   *
+   * @param itemsRequest the filter
+   * @return the items
+   */
   public Item[] getItems(ItemsRequest itemsRequest) {
     return itemRepository.getItems(itemsRequest);
   }
 
+  /**
+   * get item based on id.
+   *
+   * @param itemRequest the id
+   * @return the item
+   */
   public ItemResponse getItem(ItemRequest itemRequest) {
     Item item = itemRepository.getItem(itemRequest.getItemID());
     return ItemMapper.INSTANCE.itemToItemResponse(item);
