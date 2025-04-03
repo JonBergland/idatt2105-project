687d12846a93bb6b877e5b30a219e9c3f91caa65
Vetle <vetletb@stud.ntnu.no>
2025-04-03 11:21:33 +0200

    style: removed unused imports in ItemsResponse

--- backend/src/main/java/edu/ntnu/idatt2105/backend/item/dto/ItemsResponse.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/dto/ItemsResponse.java
@@ -1,11 +1,8 @@
 package edu.ntnu.idatt2105.backend.item.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 /**
