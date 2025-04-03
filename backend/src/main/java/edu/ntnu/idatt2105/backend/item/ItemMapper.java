621c0d2c3b857f2055617f4e889ca835016a8d61
Vetle <vetletb@stud.ntnu.no>
2025-04-03 09:54:18 +0200

    docs: added Javadoc to ItemMapper

--- backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemMapper.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemMapper.java
@@ -5,11 +5,26 @@ import edu.ntnu.idatt2105.backend.item.model.Item;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * A Mapper for mapping between Item and DTOs.
+ */
 @Mapper
 public interface ItemMapper {
   ItemMapper INSTANCE = Mappers.getMapper(ItemMapper.class);
 
+  /**
+   * maps item model to itemResponse dto.
+   *
+   * @param item the item to map
+   * @return the mapped dto
+   */
   ItemResponse itemToItemResponse(Item item);
 
+  /**
+   * maps item array to itemResponse array.
+   *
+   * @param items the item array
+   * @return the itemResponse array
+   */
   ItemResponse[] itemsToItemResponses(Item[] items);
 }
