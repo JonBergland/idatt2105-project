649e04ae073c0336462b7655c02ee1e2b832f762
Vetle <vetletb@stud.ntnu.no>
2025-04-03 09:30:50 +0200

    feat: added get item endpoint

--- backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemController.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemController.java
@@ -1,16 +1,20 @@
 package edu.ntnu.idatt2105.backend.item;
 
+import edu.ntnu.idatt2105.backend.item.dto.ItemRequest;
 import edu.ntnu.idatt2105.backend.item.dto.ItemResponse;
 import edu.ntnu.idatt2105.backend.item.dto.ItemsRequest;
 import edu.ntnu.idatt2105.backend.item.dto.ItemsResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/api/store/item")
@@ -26,5 +30,16 @@ public class ItemController {
   public ItemsResponse getItems(@RequestBody ItemsRequest itemsRequest) {
     ItemResponse[] itemResponses = ItemMapper.INSTANCE.itemsToItemResponses(itemService.getItems(itemsRequest));
     return new ItemsResponse(itemResponses);
+  }
+
+  @PostMapping("/get")
+  public ItemResponse itemResponses(@RequestBody ItemRequest itemRequest) {
+    try {
+      logger.info("getting item with id: {}", itemRequest.getItemID());
+      return itemService.getItem(itemRequest);
+    } catch (DataAccessException e) {
+      logger.warn("could not retrieve item: {}", e.getMessage());
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    }
   }
 }
