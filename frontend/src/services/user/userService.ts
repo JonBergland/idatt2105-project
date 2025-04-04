effccb87a484a0f334962dcbfdbf0466aaf18c37
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-04 09:55:01 +0200

    docs: add docs to methods in userService

--- frontend/src/services/user/userService.ts
+++ frontend/src/services/user/userService.ts
@@ -1,19 +1,40 @@
-import type { UserLoginDTO, UserRegistrationDTO } from "@/models/user";
+import type { User, UserLoginDTO, UserRegistrationDTO } from "@/models/user";
 import axiosInstance from "@/services/axiosService";
 
 
+/**
+ * A service class for handling user-related operations such as registration and login.
+ */
 class UserService {
 
-  getUserInfo() {
-    // TODO
+  /**
+   * Retrieves information about the currently authenticated user.
+   * @returns A promise that resolves with the user's information.
+   */
+  async getUserInfo(): Promise<User> {
+    // Not implemented in backend
+    const response = await axiosInstance.get<User>('/user/info');
+    return response.data;
   }
 
-  async registerUser(user: UserRegistrationDTO) {
-    return await axiosInstance.post<UserRegistrationDTO>('/token/signup', user)
+  /**
+   * Registers a new user in the system.
+   * @param user  An {@link UserRegistrationDTO} containing the user's registration details.
+   * @returns     A promise containing the status of the signup
+   */
+  async registerUser(user: UserRegistrationDTO): Promise<boolean> {
+    const response = await axiosInstance.post<boolean>('/token/signup', user);
+    return response.data;
   }
 
-  async loginUser(user: UserLoginDTO) {
-    return await axiosInstance.post<UserLoginDTO>('/token/signin', user)
+  /**
+   * Logs in a user with the provided credentials.
+   * @param user  An {@link UserLoginDTO} containing the user's login credentials.
+   * @returns     A promise containing the status of the login
+   */
+  async loginUser(user: UserLoginDTO): Promise<boolean> {
+    const response = await axiosInstance.post<boolean>('/token/signin', user)
+    return response.data
   }
 }
 
