dd02e758e3a123a19cd34fd1d33daa95d90e65bf
florianleucht <fsleucht@stud.ntnu.no>
2025-04-04 10:54:49 +0200

    feat: renamed RecommendationsGrid to ItemGroup and added support for column view

--- frontend/src/components/Home/ItemGroup.vue
+++ frontend/src/components/Home/ItemGroup.vue
@@ -16,6 +16,11 @@ const props = defineProps({
     type: Array as () => Item[],
     required: false,
   },
+  mode: {
+    type: String,
+    default: 'Grid',
+    validator: (value: string) => ['Grid', 'Column'].includes(value),
+  },
 });
 
 /**
@@ -23,40 +28,46 @@ const props = defineProps({
  *
  * @param {number} itemId - The unique identifier of the clicked item.
  */
- function handleItemClick(itemId: number) {
+function handleItemClick(itemId: number) {
   emit('item-clicked', itemId);
 }
 </script>
 
 <template>
-  <div class="item-grid">
+  <div :class="['item-container', mode === 'Grid' ? 'item-grid' : 'item-column']">
     <ItemCard
-    v-for="(item, index) in props.items"
-    :key="index"
-    :item="item"
-    @clicked-item="handleItemClick"
+      v-for="(item, index) in props.items"
+      :key="index"
+      :item="item"
+      :size="mode === 'Grid' ? 'narrow' : 'full'"
+      @clicked-item="handleItemClick"
     />
   </div>
 </template>
 
 <style scoped>
+.item-container {
+  width: 100%;
+}
+
 .item-grid {
   display: flex;
   justify-content: center;
   align-items: flex-start;
   align-content: flex-start;
   gap: 24px;
-  flex: 1 0 0;
   flex-wrap: wrap;
+}
+
+.item-column {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 }
 
 @media (max-width: 768px) {
   .item-grid {
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-  }
-
-  .item-card {
-    width: 150px;
   }
 }
 </style>
