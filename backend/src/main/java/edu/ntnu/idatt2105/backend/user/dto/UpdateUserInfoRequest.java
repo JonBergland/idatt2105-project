35051304e87d841e863f3081da0bcb5c131c63b8
Vetle <vetletb@stud.ntnu.no>
2025-04-05 11:29:49 +0200

    docs: added Javadoc to UpdateUserInfoRequest

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/UpdateUserInfoRequest.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/UpdateUserInfoRequest.java
@@ -3,6 +3,9 @@ package edu.ntnu.idatt2105.backend.user.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * a DTO for updating the user's info.
+ */
 @Getter
 @Setter
 public class UpdateUserInfoRequest {
