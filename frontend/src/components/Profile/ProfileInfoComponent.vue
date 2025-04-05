2234e1bd820f6bd189ae20874d098d4b915a52e7
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-05 23:12:42 +0200

    feat: initialize ProfileInfoComponent

---
+++ frontend/src/components/Profile/ProfileInfoComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import profileImage from '@/assets/icons/profile-avatar.svg'
+</script>
+<template>
+  <div class="profile-info-wrapper">
+    <div class="profile-image">
+      <img :src="profileImage" alt="profileImage" />
+    </div>
+    <div class="profile-details">
+      <div class="profile-info"></div>
+      <div class="profile-buttons">
+        <button class="edit-info-button"></button>
+        <button class="logout-button"> </button> <!-- Add a popup-window for logout?  -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.profile-info-wrapper {
+}
+</style>
