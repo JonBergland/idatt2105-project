26ba3376eca99760fe8c218495ef39b9a58afe9d
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-05 23:13:01 +0200

    feat: initialize ProfileView

---
+++ frontend/src/views/Profile/ProfileView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+<template>
+<div class="user-profile-wrapper">
+  <!-- Profile info -->
+
+  <!-- User or Admin profile specific  -->
+</div>
+
+</template>
+
+<script>
+
+</script>
