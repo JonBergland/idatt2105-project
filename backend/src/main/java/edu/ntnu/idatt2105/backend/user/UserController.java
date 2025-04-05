26715a1002614a652f99e9fc92b54bfc9d012e02
Vetle <vetletb@stud.ntnu.no>
2025-04-05 11:30:30 +0200

    docs: added Javadoc to UserController

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/UserController.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/UserController.java
@@ -5,7 +5,6 @@ import edu.ntnu.idatt2105.backend.user.dto.AddItemRequest;
 import edu.ntnu.idatt2105.backend.user.dto.EditItemRequest;
 import edu.ntnu.idatt2105.backend.user.dto.GetUserInfoResponse;
 import edu.ntnu.idatt2105.backend.user.dto.UpdateUserInfoRequest;
-import edu.ntnu.idatt2105.backend.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * a controller for user specific operations
+ */
 @RestController
 @RequestMapping(value = "/api/user")
 @CrossOrigin
@@ -27,12 +29,22 @@ public class UserController {
 
   private final UserService userService;
 
+  /**
+   * endpoint for editing a user's info.
+   *
+   * @param updateUserInfoRequest new user info
+   */
   @PostMapping("/info")
   public void updateUserInfo(@RequestBody UpdateUserInfoRequest updateUserInfoRequest) {
     logger.info("hei");
     userService.editUser(updateUserInfoRequest);
   }
 
+  /**
+   * endpoint for getting a user's info.
+   *
+   * @return the user info to send
+   */
   @GetMapping("/info")
   public GetUserInfoResponse getUserInfo() {
     String userID = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -54,16 +66,31 @@ public class UserController {
 
   }
 
+  /**
+   * endpoint for getting a user's items.
+   *
+   * @return the user's items
+   */
   @GetMapping("/item")
   public ItemsResponse getUserItems() {
     return userService.getUserItems();
   }
 
+  /**
+   * endpoint for user to add item.
+   *
+   * @param addItemRequest the item to add
+   */
   @PostMapping("/item")
   public void addUserItem(@RequestBody AddItemRequest addItemRequest) {
     userService.addUserItem(addItemRequest);
   }
 
+  /**
+   * endpoint for editing a user's item.
+   *
+   * @param editItemRequest the updated item
+   */
   @PostMapping("/item/edit")
   public void editUserItem(@RequestBody EditItemRequest editItemRequest) {
     userService.editUserItem(editItemRequest);
