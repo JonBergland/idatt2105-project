0c3940e240d1cd6af7591080b60d3853605fb02a
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-06 12:17:21 +0200

    feat: implement ProfileInfoComponent with user details and action buttons

--- frontend/src/components/Profile/ProfileInfoComponent.vue
+++ frontend/src/components/Profile/ProfileInfoComponent.vue
@@ -1,5 +1,48 @@
 <script setup lang="ts">
 import profileImage from '@/assets/icons/profile-avatar.svg'
+import type { User } from '@/models/user';
+import { ref } from 'vue';
+import UserInfoComponent from '@/components/Profile/UserInfoComponent.vue';
+import ProfileButtonsComponent from '@/components/Profile/ProfileButtonsComponent.vue';
+
+defineProps<{
+  user: User | null
+}>()
+
+const emit = defineEmits(["saveUser", "logoutUser"])
+
+const isEditing = ref(false)
+const isLoggingOut = ref(false)
+
+function handleEditMode() {
+  isEditing.value = true
+  isLoggingOut.value = false
+}
+
+function handleLogoutMode() {
+  isEditing.value = false
+  isLoggingOut.value = true
+}
+
+function handleSave() {
+  isEditing.value = false
+  isLoggingOut.value = false
+
+  // TODO: add logic for updating user info
+}
+
+function handleLogout() {
+  isEditing.value = false
+  isLoggingOut.value = false
+
+  emit("logout")
+}
+
+function handleCancel() {
+  isEditing.value = false
+  isLoggingOut.value = false
+}
+
 </script>
 <template>
   <div class="profile-info-wrapper">
@@ -7,16 +50,32 @@ import profileImage from '@/assets/icons/profile-avatar.svg'
       <img :src="profileImage" alt="profileImage" />
     </div>
     <div class="profile-details">
-      <div class="profile-info"></div>
-      <div class="profile-buttons">
-        <button class="edit-info-button"></button>
-        <button class="logout-button"> </button> <!-- Add a popup-window for logout?  -->
-      </div>
+      <UserInfoComponent
+        :first-name="user?.name"
+        :last-name="user?.surname"
+        :email="user?.email"
+        :phoneNumber="'+' + user?.countryCode + ' ' + user?.phoneNumber"
+        :location="user?.city"
+      />
+      <ProfileButtonsComponent
+      :editMode = isEditing
+      :logoutMode = isLoggingOut
+      @edit-mode="handleEditMode"
+      @logout-mode="handleLogoutMode"
+      @save="handleSave"
+      @logout="handleLogout"
+      @cancel="handleCancel"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
 .profile-info-wrapper {
+  display: flex;
+  gap: 20px;
+  align-items: flex-start;
+  width: 100%;
+  margin-top: 20px;
 }
 </style>
