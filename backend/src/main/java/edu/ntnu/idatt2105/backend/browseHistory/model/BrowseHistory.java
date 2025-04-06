ad9e379959a1073aafb84d9339a3a8e5237d515f
Vetle <vetletb@stud.ntnu.no>
2025-04-06 13:58:07 +0200

    feat: created BrowseHistory model

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/browseHistory/model/BrowseHistory.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2105.backend.browseHistory.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BrowseHistory {
+  private int userID;
+  private int itemID;
+}
