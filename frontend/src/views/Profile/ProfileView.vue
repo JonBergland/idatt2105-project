f418e00c53bb3c66dbadf1847dfb46c597626393
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-06 12:18:09 +0200

    feat: display user profile information in ProfileView

--- frontend/src/views/Profile/ProfileView.vue
+++ frontend/src/views/Profile/ProfileView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import ProfileInfoComponent from "@/components/Profile/ProfileInfoComponent.vue";
+import type { User } from "@/models/user";
+import { useAuthStore } from "@/stores/authStore";
+import { onMounted, ref } from "vue";
+
+const authStore = useAuthStore()
+const user = ref<User | null>(null);
+
+onMounted(() => {
+  if (authStore.isAuth) {
+    user.value = authStore.userData;
+  }
+});
 
 </script>
 <template>
 <div class="user-profile-wrapper">
   <!-- Profile info -->
+   <ProfileInfoComponent
+   :user = user
+   />
+
 
   <!-- User or Admin profile specific  -->
 </div>
