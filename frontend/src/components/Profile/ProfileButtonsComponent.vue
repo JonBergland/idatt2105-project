a050b31cc346c23c536c997bf9553ccde6b59819
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-06 13:37:37 +0200

    feat: add emits to button-clicks in ProfileButtonComponent

--- frontend/src/components/Profile/ProfileButtonsComponent.vue
+++ frontend/src/components/Profile/ProfileButtonsComponent.vue
@@ -8,22 +8,31 @@ const emit = defineEmits(['editMode', 'logoutMode', 'save', 'logout', 'cancel'])
 </script>
 
 <template>
-  <div v-if="!editMode && !logoutMode" class="profile-buttons-container">
-        <button class="edit-info-button">Edit info</button>
-        <button class="logout-button">Log Out</button>
+  <div v-if="!editMode && !logoutMode" class="profile-buttons">
+    <p></p>
+    <div class="buttons-container">
+      <button class="edit-info-button" @click="emit('editMode')">Edit info</button>
+      <button class="logout-button" @click="emit('logoutMode')">Log Out</button>
+    </div>
   </div>
 
-  <div v-else-if="editMode && !logoutMode" class="profile-buttons-container">
-        <button class="save-info">Save</button>
-        <button class="cancel-button"> Cancel </button>
+  <div v-else-if="editMode && !logoutMode" class="profile-buttons">
+    <p></p>
+    <div class="buttons-container">
+      <button class="save-info" @click="emit('save')">Save</button>
+      <button class="cancel-button" @click="emit('cancel')">Cancel</button>
+    </div>
   </div>
 
-  <div v-else-if="!editMode && logoutMode" class="profile-buttons-container">
-        <button class="logout">Logout</button>
-        <button class="cancel-button"> Cancel </button>
+  <div v-else-if="!editMode && logoutMode" class="profile-buttons">
+    <p>Are you sure you want to sign out?</p>
+    <div class="buttons-container">
+      <button class="logout" @click="emit('logout')">Yes</button>
+      <button class="cancel-button" @click="emit('cancel')">Cancel</button>
+    </div>
   </div>
 
-  <div v-else class="profile-buttons-container"></div>
+  <div v-else class="profile-buttons"></div>
 
 </template>
 
