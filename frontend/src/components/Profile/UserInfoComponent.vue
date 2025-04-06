7657c6179c2dada3f22679a057ea50666b74249b
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-06 10:27:12 +0200

    feat: initialize UserInfoComponent

---
+++ frontend/src/components/Profile/UserInfoComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  firstName: string | undefined,
+  lastName: string | undefined,
+  email: string | undefined,
+  phoneNumber: string | undefined,
+  location: string | undefined
+}>()
+</script>
+
+<template>
+  <div class="user-info">
+        <h1>{{ firstName + " " + lastName }}</h1>
+        <h3>{{ "First name: " + firstName }}</h3>
+        <h3>{{ "Last name: " + lastName }}</h3>
+        <h3>{{ "Email: " + email }}</h3>
+        <h3>{{ "Phone number: " + phoneNumber }}</h3>
+        <h3>{{ "Location: " + location }}</h3>
+      </div>
+</template>
+
+
