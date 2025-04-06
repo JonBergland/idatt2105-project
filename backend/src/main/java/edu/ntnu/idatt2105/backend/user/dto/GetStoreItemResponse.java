b04f6325b318557fe3bac21c380d9346dbdbb580
Vetle <vetletb@stud.ntnu.no>
2025-04-06 13:57:21 +0200

    feat: created GetStoreItemResponse

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/GetStoreItemResponse.java
@@ -0,0 +1,21 @@
+package edu.ntnu.idatt2105.backend.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetStoreItemResponse {
+  int itemID;
+  String name;
+  String category;
+  String state;
+  String seller;
+  String description;
+  String published;
+  int price;
+}
