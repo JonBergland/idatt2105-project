21a47f95d9b67b64d1907534de1b86127e0a72a3
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-06 19:11:52 +0200

    feat: change side to login when no user is set

--- frontend/src/views/Profile/ProfileView.vue
+++ frontend/src/views/Profile/ProfileView.vue
@@ -2,18 +2,41 @@
 import ProfileInfoComponent from "@/components/Profile/ProfileInfoComponent.vue";
 import type { User } from "@/models/user";
 import { useAuthStore } from "@/stores/authStore";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
+import { useRouter } from 'vue-router';
 
-const authStore = useAuthStore()
+const authStore = useAuthStore();
+const router = useRouter();
 const user = ref<User | null>(null);
 
-onMounted(() => {
-  if (authStore.isAuth) {
-    user.value = authStore.userData;
+onMounted(async () => {
+  if (!authStore.isAuth) {
+    try {
+      const isAuthenticated = await authStore.checkIfAuth();
+      if (!isAuthenticated) {
+        router.push('/login');
+        return;
+      }
+    } catch (error) {
+      console.error("Error checking authentication:", error);
+      router.push('/login');
+      return;
+    }
   }
+
+    // Set user data from auth store
+    user.value = authStore.userData;
 });
 
+watch(
+  () => authStore.userData,
+  (newUserData) => {
+    user.value = newUserData;
+  },
+  { deep: true }
+);
 </script>
+
 <template>
 <div class="user-profile-wrapper">
   <!-- Profile info -->
