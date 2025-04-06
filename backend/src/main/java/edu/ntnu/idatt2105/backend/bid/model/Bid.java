5ac9b713f366985c7db16e5af5a2cd2678a35a52
Vetle <vetletb@stud.ntnu.no>
2025-04-06 18:33:23 +0200

    feat: created Bid model

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/bid/model/Bid.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2105.backend.bid.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bid {
+  private int itemID;
+  private int userID;
+  private int askingPrice;
+  private String status;
+}
