3e7e11f02add8f0ef5a85fe4d0bb6f15264a120c
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-06 12:16:53 +0200

    feat: initialize ProfileButtonsComponent

---
+++ frontend/src/components/Profile/ProfileButtonsComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  editMode: boolean,
+  logoutMode: boolean
+}>()
+
+const emit = defineEmits(['editMode', 'logoutMode', 'save', 'logout', 'cancel'])
+</script>
+
+<template>
+  <div v-if="!editMode && !logoutMode" class="profile-buttons-container">
+        <button class="edit-info-button">Edit info</button>
+        <button class="logout-button">Log Out</button>
+  </div>
+
+  <div v-else-if="editMode && !logoutMode" class="profile-buttons-container">
+        <button class="save-info">Save</button>
+        <button class="cancel-button"> Cancel </button>
+  </div>
+
+  <div v-else-if="!editMode && logoutMode" class="profile-buttons-container">
+        <button class="logout">Logout</button>
+        <button class="cancel-button"> Cancel </button>
+  </div>
+
+  <div v-else class="profile-buttons-container"></div>
+
+</template>
+
+<style>
+.profile-buttons-container {
+  display: flex;
+  gap: 20px;
+  margin-top: 10px;
+}
+</style>
