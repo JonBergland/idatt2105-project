be086aff90434dbd77f5062db0976c0d5baa02a7
Vetle <vetletb@stud.ntnu.no>
2025-04-06 13:57:50 +0200

    feat: map between new dto and item in ItemMapper

--- backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemMapper.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/ItemMapper.java
@@ -4,6 +4,7 @@ import edu.ntnu.idatt2105.backend.item.dto.ItemResponse;
 import edu.ntnu.idatt2105.backend.item.model.Item;
 import edu.ntnu.idatt2105.backend.user.dto.AddItemRequest;
 import edu.ntnu.idatt2105.backend.user.dto.EditItemRequest;
+import edu.ntnu.idatt2105.backend.user.dto.GetStoreItemResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -63,4 +64,14 @@ public interface ItemMapper {
   @Mapping(source = "description", target = "description")
   @Mapping(source = "itemID", target = "itemID")
   Item editItemRequestToItem(EditItemRequest editItemRequest);
+
+  @Mapping(source = "category", target = "category")
+  @Mapping(source = "itemID", target = "itemID")
+  @Mapping(source = "seller", target = "seller")
+  @Mapping(source = "name", target = "name")
+  @Mapping(source = "published", target = "published")
+  @Mapping(source = "price", target = "price")
+  @Mapping(source = "description", target = "description")
+  @Mapping(source = "state", target = "state")
+  GetStoreItemResponse itemToGetStoreItemResponse(Item item);
 }
