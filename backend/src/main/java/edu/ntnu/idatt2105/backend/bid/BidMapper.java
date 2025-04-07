d57dcd5d65cd78df0d467f264c8e95e41b31c11c
Vetle <vetletb@stud.ntnu.no>
2025-04-07 10:19:41 +0200

    feat: added new dto to BidMapper

--- backend/src/main/java/edu/ntnu/idatt2105/backend/bid/BidMapper.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/bid/BidMapper.java
@@ -2,6 +2,7 @@ package edu.ntnu.idatt2105.backend.bid;
 
 import edu.ntnu.idatt2105.backend.bid.model.Bid;
 import edu.ntnu.idatt2105.backend.user.dto.GetBidItemResponse;
+import edu.ntnu.idatt2105.backend.user.dto.GetBidsResponse;
 import edu.ntnu.idatt2105.backend.user.dto.PlaceBidRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -20,5 +21,13 @@ public interface BidMapper {
   GetBidItemResponse bidToGetBidItemResponse(Bid bid);
 
   GetBidItemResponse[] bidArrayToGetBidItemResponseArray(Bid[] bid);
+
+  @Mapping(source = "itemID", target = "itemID")
+  @Mapping(source = "askingPrice", target = "askingPrice")
+  @Mapping(source = "status", target = "status")
+  @Mapping(source = "published", target = "published")
+  GetBidsResponse bidToGetBidsResponse(Bid bid);
+
+  GetBidsResponse[] bidArrayToGetBidsResponseArray(Bid[] bid);
 
 }
