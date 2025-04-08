353954b2d00c4b825842ee14eb2db415ade758f6
Vetle <vetletb@stud.ntnu.no>
2025-04-08 08:40:40 +0200

    feat: added set bid status and owner check to BidRepository

--- backend/src/main/java/edu/ntnu/idatt2105/backend/bid/BidRepository.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/bid/BidRepository.java
@@ -3,8 +3,10 @@ package edu.ntnu.idatt2105.backend.bid;
 import edu.ntnu.idatt2105.backend.bid.model.Bid;
 import edu.ntnu.idatt2105.backend.item.model.Item;
 import edu.ntnu.idatt2105.backend.user.dto.GetBidsRequest;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -34,7 +36,7 @@ public class BidRepository {
 
   public Bid[] getItemBids(int userID, GetBidsRequest getBidsRequest) {
     List<Bid> bidList = jdbcTemplate.query(
-        "SELECT * FROM Bids WHERE user_id = ? AND item_id = ? "
+        "SELECT *, item_id AS itemID, id AS bidID FROM Bids WHERE user_id = ? AND item_id = ? "
             + "ORDER BY published DESC "
             + "LIMIT ? OFFSET ?",
         new Object[]{
@@ -44,5 +46,30 @@ public class BidRepository {
             getBidsRequest.getSegmentOffset()[0] * getBidsRequest.getSegmentOffset()[1]},
         new BeanPropertyRowMapper<>(Bid.class));
     return bidList.toArray(new Bid[0]);
+  }
+
+  public void setBidStatus(Bid bid, int userID) throws AccessDeniedException {
+    if (ownsBidItem(userID, bid.getBidID())) {
+      jdbcTemplate.update(
+          "UPDATE Bids "
+              + "SET status = ? "
+              + "WHERE id = ?",
+          bid.getStatus(),
+          bid.getBidID());
+    } else {
+      throw new AccessDeniedException("the user don't own this item");
+    }
+  }
+
+  private boolean ownsBidItem(int userID, int bidID) {
+    return jdbcTemplate.queryForObject(
+        "SELECT IF(EXISTS( "
+            + "SELECT 1 "
+            + "FROM Item "
+            + "JOIN Bids ON Item.id = Bids.item_id "
+            + "WHERE Item.user_id = ? AND Bids.id = ? "
+            + "), 'true', 'false') AS finnes",
+        new Object[]{userID, bidID},
+        Boolean.class);
   }
 }
