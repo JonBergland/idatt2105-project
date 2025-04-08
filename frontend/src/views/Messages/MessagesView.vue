e67ad012f36e8d0c775ead95e6a3ce1d592c26a2
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-08 10:15:24 +0200

    feat: initialize messagesView

---
+++ frontend/src/views/Messages/MessagesView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+<template>
+  <!-- Div if screen is wide enough -->
+  <div class="messages-page-wrapper">
+    <div class="messages-list">
+      <!-- Messages list component -->
+    </div>
+    <div class="chat-view">
+      <!-- Chat view -->
+    </div>
+  </div>
+</template>
+<style>
+</style>
