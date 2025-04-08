904694688a1cb553bcc151976ff87558dde29b05
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-08 10:16:01 +0200

    feat: initialize MessageListComponent

---
+++ frontend/src/components/Messages/MessagesListComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  messages: string[] // Create DTO
+}>()
+</script>
+<template>
+  <div class="messages-list-wrapper">
+    <!-- Header -->
+     <h1>Messages</h1>
+     <div class="messages-list">
+      <!-- List of messages -->
+     </div>
+
+  </div>
+
+</template>
+<style>
+</style>
