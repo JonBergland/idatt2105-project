92c26aaf364aa209605892a81a1fb5bbde82b53c
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-08 13:49:16 +0200

    feat: add senderID

--- frontend/src/models/message.ts
+++ frontend/src/models/message.ts
@@ -3,6 +3,7 @@ import type { ItemResponseDTO } from "@/models/item";
 
 export interface Message {
   messageID: number;
+  senderID: number;         // The ID of the user that sent the message
   message: string;
   notSeenByUser: boolean; // Is true when the user has not seen the message
   published: string; // Datetime of the object?
