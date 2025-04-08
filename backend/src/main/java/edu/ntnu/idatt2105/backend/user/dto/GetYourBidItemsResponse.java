f15fb1d0391e55f51047e4436ee274f936ae4bf1
Vetle <vetletb@stud.ntnu.no>
2025-04-08 11:03:06 +0200

    feat: created GetYourBidItemsResponse dto

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/GetYourBidItemsResponse.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2105.backend.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetYourBidItemsResponse {
+  int itemID;
+  int userID;
+  String itemName;
+  String email;
+}
