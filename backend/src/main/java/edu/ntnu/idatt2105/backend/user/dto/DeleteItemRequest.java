eb810423171eb183601a1980ce0597d1baf2eef9
Vetle <vetletb@stud.ntnu.no>
2025-04-09 13:35:54 +0200

    docs: added Javadoc to DeleteItemRequest

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/DeleteItemRequest.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/DeleteItemRequest.java
@@ -3,6 +3,9 @@ package edu.ntnu.idatt2105.backend.user.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * a DTO for deleting an item.
+ */
 @Getter
 @Setter
 public class DeleteItemRequest {
