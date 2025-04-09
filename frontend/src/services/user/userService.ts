9d6dd373d5b9973f505cda37a575cb825531baf6
Jon Bergland <jobergl@stud.ntnu.no>
2025-04-09 23:24:31 +0200

    feat: add bid-methods in userService

--- frontend/src/services/user/userService.ts
+++ frontend/src/services/user/userService.ts
@@ -1,4 +1,4 @@
-import type { BidsOnItemByUserRequest, BidsOnItemByUserResponse, PlaceBid } from "@/models/bid";
+import type { BidOnItemByUserRequest, BidOnItemByUserResponse, BidsFromUsersOnUserItemRequest, BidsFromUsersOnUserItemResponse, PlaceBid, UserBidItemsRequest, UserBidItemsResponse, UsersWithBidOnUserItemRequest, UsersWithBidOnUserItemResponse } from "@/models/bid";
 import type { ItemsResponseDTO, ItemRequestDTO, ItemResponseDTO } from "@/models/item";
 import type { User, UserLoginDTO, UserRegistrationDTO, AddItemRequest, UpdateItemRequest } from "@/models/user";
 import axiosInstance from "@/services/axiosService";
@@ -138,7 +138,7 @@ class UserService {
    */
   async postUserBid(bid: PlaceBid): Promise<number> {
     try {
-      await axiosInstance.post<PlaceBid>('/user/item/bid/place', bid);
+      await axiosInstance.post('/user/item/bid/place', bid);
       return 1;
     } catch (error) {
       console.error('Error placing bid on item:', error);
@@ -147,17 +147,67 @@ class UserService {
   }
 
   /**
+   * Retrieves a list of items that the user has placed bids on.
+   *
+   * @param request  An object containing the request parameters for fetching user bid items.
+   * @returns        A promise that resolves to an array of (@link UserBidItemsResponse) objects or
+   *                 empty response in case of an error
+   *
+   */
+  async getItemsWithUserBids(request: UserBidItemsRequest): Promise<UserBidItemsResponse[] | null> {
+    try {
+      const response = await axiosInstance.post<UserBidItemsResponse[]>('/user/item/bids', request);
+      return response.data
+    } catch (error) {
+      console.error('Error getting items with user bids: ', error)
+      return null;
+    }
+  }
+
+  /**
    * Retrieves bids placed by a user on an item.
    *
    * @param request The request object containing the item details.
-   * @returns       A promise that resolves to a `BidsOnItemByUserResponse` or an empty response in case of an error.
+   * @returns       A promise that resolves to a (@link BidsOnItemByUserResponse) or an empty response in case of an error.
    */
-  async getUserBids(request: BidsOnItemByUserRequest): Promise<BidsOnItemByUserResponse | null> {
+  async getUserBidsOnItem(request: BidOnItemByUserRequest): Promise<BidOnItemByUserResponse[] | null> {
     try {
-      const response = await axiosInstance.post<BidsOnItemByUserResponse>('/item/bids/item', request);
+      const response = await axiosInstance.post<BidOnItemByUserResponse[]>('/user/item', request);
       return response.data;
     } catch (error) {
-      console.error('Error getting bids on Item by User: ', error);
+      console.error('Error getting bids on item by User: ', error);
+      return null;
+    }
+  }
+
+  /**
+   * Retrieves users with bid on an user item.
+   *
+   * @param request The request object containing the segment offset.
+   * @returns       A promise that resolves to a (@link UsersWithBidOnUserItemResponse) or an empty response in case of an error.
+   */
+  async getUsersWithBidOnUserItem(request: UsersWithBidOnUserItemRequest): Promise<UsersWithBidOnUserItemResponse[] | null> {
+    try {
+      const response = await axiosInstance.post<UsersWithBidOnUserItemResponse[]>('/user/item/bid/users', request);
+      return response.data;
+    } catch (error) {
+      console.error('Error getting Users with bids user items: ', error);
+      return null;
+    }
+  }
+
+  /**
+   * Retrieves bids from other users on the logged in user's item.
+   *
+   * @param request The request object containing the segment offset.
+   * @returns       A promise that resolves to a (@link BidsFromUsersOnUserItemResponse) or an empty response in case of an error.
+   */
+  async getBidsFromUsersOnUserItem(request: BidsFromUsersOnUserItemRequest): Promise<BidsFromUsersOnUserItemResponse[] | null> {
+    try {
+      const response = await axiosInstance.post<BidsFromUsersOnUserItemResponse[]>('/user/item/bid/users', request);
+      return response.data;
+    } catch (error) {
+      console.error('Error getting bids from users on user items: ', error);
       return null;
     }
   }
