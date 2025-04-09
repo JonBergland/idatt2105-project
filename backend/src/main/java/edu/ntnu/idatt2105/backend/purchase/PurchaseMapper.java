f476c35cae995a7d513612590a176d120434f655
Vetle <vetletb@stud.ntnu.no>
2025-04-09 15:49:45 +0200

    docs: added Javadoc to PurchaseMapper

--- backend/src/main/java/edu/ntnu/idatt2105/backend/purchase/PurchaseMapper.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/purchase/PurchaseMapper.java
@@ -7,10 +7,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * A Mapper for mapping between Purchase and DTOs.
+ */
 @Mapper
 public interface PurchaseMapper {
   PurchaseMapper INSTANCE = Mappers.getMapper(PurchaseMapper.class);
 
+  /**
+   * maps buyItemRequest dto to purchase model.
+   *
+   * @param buyItemRequest the dto
+   * @return the mapped purchase
+   */
   @Mapping(source = "itemID", target = "itemID")
   Purchase buyItemRequestToPurchase(BuyItemRequest buyItemRequest);
 }
