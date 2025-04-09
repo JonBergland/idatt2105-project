a141d5569fa34d058af7f5aa378b0db945a482f8
Vetle <vetletb@stud.ntnu.no>
2025-04-09 12:25:22 +0200

    docs: added Javadoc to GetBookmarkedItemsResponse

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/GetBookmarkedItemsResponse.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/GetBookmarkedItemsResponse.java
@@ -3,6 +3,9 @@ package edu.ntnu.idatt2105.backend.user.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * A DTO for responding to bookmarked items request.
+ */
 @Getter
 @Setter
 public class GetBookmarkedItemsResponse {
