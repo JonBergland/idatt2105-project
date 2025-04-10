13a000680aeeef99b761707a4f7258a31bd579fa
Vetle <vetletb@stud.ntnu.no>
2025-04-10 09:01:05 +0200

    docs: Swagger documentation to class and fields in BuyItemRequest

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/BuyItemRequest.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/BuyItemRequest.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2105.backend.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,8 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Schema(description = "Data transfer object for buying items")
 public class BuyItemRequest {
+  @Schema(description = "item id of item being bought", example = "51")
   int itemID;
 }
