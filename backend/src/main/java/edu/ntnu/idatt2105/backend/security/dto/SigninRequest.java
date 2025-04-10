cbda271b047e8c168b3bff08521d95975bb74df3
Vetle <vetletb@stud.ntnu.no>
2025-04-10 08:57:41 +0200

    docs: Swagger documentation to class and fields in SigninRequest

--- backend/src/main/java/edu/ntnu/idatt2105/backend/security/dto/SigninRequest.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/security/dto/SigninRequest.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2105.backend.security.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -14,10 +15,13 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Data transfer object for user sign in")
 public class SigninRequest {
   @Email
   @NotBlank
+  @Schema(description = "Email of the user", example = "Joe@example.org")
   private String email;
   @NotBlank
+  @Schema(description = "Password of the user", example = "Password123")
   private String password;
 }
