475feb415b5e003141e2b7a372270d1daad5bff7
Vetle <vetletb@stud.ntnu.no>
2025-04-10 14:36:06 +0200

    docs: add missing Javadoc in SwaggerConfig

--- backend/src/main/java/edu/ntnu/idatt2105/backend/swagger/SwaggerConfig.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/swagger/SwaggerConfig.java
@@ -5,9 +5,17 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Swagger configuration class for the Yard API.
+ */
 @Configuration
 public class SwaggerConfig {
 
+  /**
+   * Configures the OpenAPI documentation for the Yard API.
+   *
+   * @return an OpenAPI object with the API information
+   */
   @Bean
   public OpenAPI customOpenAPI() {
     return new OpenAPI()
