39805867ee8476453159896ca646c117ae21688a
Vetle <vetletb@stud.ntnu.no>
2025-04-10 09:03:05 +0200

    docs: Swagger documentation to class and fields in GetStoreItemResponse

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/GetStoreItemResponse.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/GetStoreItemResponse.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2105.backend.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,24 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Data transfer object for responding with item with user specific info")
 public class GetStoreItemResponse {
+  @Schema(description = "item id", example = "74")
   int itemID;
+  @Schema(description = "item name", example = "Hammer")
   String name;
+  @Schema(description = "item category", example = "verktøy")
   String category;
+  @Schema(description = "item state", example = "sold")
   String state;
+  @Schema(description = "seller email", example = "seller@sale.com")
   String seller;
+  @Schema(description = "item description", example = "Stor hammer")
   String description;
+  @Schema(description = "item published", example = "2025-04-02 12:53:20")
   String published;
+  @Schema(description = "item price", example = "195")
   int price;
+  @Schema(description = "is bookmarked", example = "false")
   boolean bookmark;
 }
