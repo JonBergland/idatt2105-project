e7ae6b0ea8f4ecca71472801c17a85963ffdef16
Vetle <vetletb@stud.ntnu.no>
2025-04-10 08:58:31 +0200

    docs: Swagger documentation to class and fields in ItemRequest

--- backend/src/main/java/edu/ntnu/idatt2105/backend/item/dto/ItemRequest.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/item/dto/ItemRequest.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2105.backend.item.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,8 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Schema(description = "Data transfer object for requesting an items info")
 public class ItemRequest {
+  @Schema(description = "item id", example = "12")
   int itemID;
 }
