61e846ce37544754e7272ad485dab87bd41c5baa
Vetle <vetletb@stud.ntnu.no>
2025-04-10 14:33:18 +0200

    docs: add missing Javadoc in BookmarkRepository

--- backend/src/main/java/edu/ntnu/idatt2105/backend/bookmark/BookmarkRepository.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/bookmark/BookmarkRepository.java
@@ -14,18 +14,34 @@ public class BookmarkRepository {
 
   private final JdbcTemplate jdbcTemplate;
 
+  /**
+   * Adds a bookmark to the database.
+   *
+   * @param bookmark the bookmark to add
+   */
   public void addBookmark(Bookmark bookmark) {
     jdbcTemplate.update("INSERT INTO Bookmark (user_id, item_id) VALUES (?, ?)",
         bookmark.getUserID(),
         bookmark.getItemID());
   }
 
+  /**
+   * Deletes a bookmark in the database.
+   *
+   * @param bookmark the bookmark to update
+   */
   public void deleteBookmark(Bookmark bookmark) {
     jdbcTemplate.update("DELETE FROM Bookmark WHERE user_id = ? AND item_id = ?",
         bookmark.getUserID(),
         bookmark.getItemID());
   }
 
+  /**
+   * Checks if a bookmark exists in the database.
+   *
+   * @param bookmark the bookmark to check
+   * @return true if the bookmark exists, false otherwise
+   */
   public boolean checkBookmark(Bookmark bookmark) {
     return jdbcTemplate.queryForObject(
         "SELECT IF(EXISTS("
